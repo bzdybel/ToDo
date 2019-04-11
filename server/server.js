dc50f486bc341eb3e3a8431c4e4ee67a61661b09
bartlomiej <bartlomiej.zdybel@gmail.com>
2019-04-11 21:47:22 +0200

    change STATUSES, debug node server, repair prettier

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
-function portEcho(port) {
-    console.log("Listen at: " + port);
-}
-const express = require('express');
-const app = express();
-port = 3006;
+const express = require('express')
+const app = express()
+const PORT = 3006
 
 app.use(express.static('../front-end'))
-app.listen(port, portEcho(port));
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}...`))
