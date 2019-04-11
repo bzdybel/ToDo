1419aad3ba98692b72268c70c26685f9832a7ba1
bartlomiej <bartlomiej.zdybel@gmail.com>
2019-04-11 22:07:41 +0200

    debug server

--- server/server.js
+++ server/server.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const PORT = 3006
 
-app.use(express.static('../front-end'))
+app.use(express.static('front-end'))
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}...`))
